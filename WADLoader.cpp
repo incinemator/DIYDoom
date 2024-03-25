748c8ad74d2cb3ed99d87b0cd833c826ee82e4a7
Incinemator <giannakzorz@gmail.com>
2024-03-25 15:11:26 +0100

    WADloader class declaration

---
+++ WADLoader.cpp
@@ -0,0 +1,17 @@
+class WADLoader
+{
+public:
+    WADLoader(std::string sWADFilePath); // We always want to make sure a WAD file is passed
+    bool LoadWAD(); // Will call other helper functions to open and load the WAD file
+
+    ~WADLoader();   // Clean up!
+
+protected:
+    bool OpenAndLoad();     // Open the file and load it to memory
+    bool ReadDirectories(); // A function what will iterate though the directory section 
+
+    std::string m_sWADFilePath; // Store the file name passed to the constructor 
+    std::ifstream m_WADFile;    // The file stream that will pint to the WAD file.
+    uint8_t *m_WADData;         // let's load the file and keep it in memory! It is just a few MBs!
+    std::vector<Directory> m_WADDirectories; //let's store all the directories in this vector.
+};
